e1655dcc4d29484ce1f76f286a5f2dcfda8a3cb2
ruudkid <adeagboibukun1@gmail.com>
2023-11-18 13:57:22 +0000

    amend security group setup

--- secgrp.tf
+++ secgrp.tf
@@ -78,8 +78,8 @@ resource "aws_security_group" "vprofile-backend-sg" {
 resource "aws_security_group_rule" "sec_group_allow_itself" {
   from_port                = 0
   protocol                 = "tcp"
-  security_group_id        = "aws_security_group.vprofile-backend-sg.id"
-  source_security_group_id = "aws_security_group.vprofile-backend-sg.id"
+  security_group_id        = aws_security_group.vprofile-backend-sg.id
+  source_security_group_id = aws_security_group.vprofile-backend-sg.id
   to_port                  = 65535
   type                     = "ingress"
 }
